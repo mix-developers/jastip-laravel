ff2a86cbe18c6882df5b3537923acaa50d3a38f1
firchun025 <firchun025@gmail.com>
2023-08-07 14:46:00 +0900

    update table price

--- resources/views/pages/component/_price_list.blade.php
+++ resources/views/pages/component/_price_list.blade.php
@@ -4,7 +4,7 @@
     </div>
 </div>
 <div class="col-12">
-    <div class="row justify-content-center">
+    {{-- <div class="row justify-content-center">
         @foreach ($package_price as $item)
             <div class="col-md-6">
                 <div class="card support-bar">
@@ -18,5 +18,32 @@
                 </div>
             </div>
         @endforeach
+    </div> --}}
+    <div class="card">
+        <div class="card-body">
+            <table class="table table-bordered ">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Tujuan</th>
+                        <th>Harga</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($package_price as $item)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $item->name }}</td>
+                            <td>Rp {{ number_format($item->price) }} / Kg</td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td class="text-center"></td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
     </div>
+
 </div>
